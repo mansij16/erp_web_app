300f29e097ac0d627e3fc499d332f91b28111329
mansij16 <mansi@beyond-blueprints.com>
2026-01-19 13:59:56 +0530

    updated env variable

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:3000/api/v1";
+  process.env.STAGE_REACT_APP_API_URL || "http://localhost:5000/api/v1";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
