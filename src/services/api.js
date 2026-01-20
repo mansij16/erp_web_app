f36d20493a5ee8464300758d653a168840afb48d
mansij16 <mansi@beyond-blueprints.com>
2026-01-20 12:19:07 +0530

    removed localhost api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 // src/services/api.js
 import axios from "axios";
 
-const API_BASE_URL =
-  process.env.STAGE_REACT_APP_API_URL || "http://localhost:5000/api/v1";
+const API_BASE_URL = process.env.STAGE_REACT_APP_API_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
