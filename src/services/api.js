2dc4cb5ad47201c9e981705e810e0211525a58d8
mansij16 <mansi@beyond-blueprints.com>
2026-01-20 12:26:45 +0530

    corrected backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from "axios";
 
-const API_BASE_URL = process.env.STAGE_REACT_APP_API_URL;
+const API_BASE_URL = process.env.STAGE_REACT_APP_API_URL || 'https://stage-backend.offsocial.in/api/v1';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
